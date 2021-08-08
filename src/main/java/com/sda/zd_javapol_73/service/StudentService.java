8b74d3631d46a3a2f3615246b51991930b514339
AmeN <pawel.reclaw@gmail.com>
2021-08-08 15:02:57 +0200

    Testing of thrown exceptions and call counts

--- src/main/java/com/sda/zd_javapol_73/service/StudentService.java
+++ src/main/java/com/sda/zd_javapol_73/service/StudentService.java
@@ -1,15 +1,20 @@
 package com.sda.zd_javapol_73.service;
 
+import com.sda.zd_javapol_73.model.Grade;
 import com.sda.zd_javapol_73.model.Student;
 import com.sda.zd_javapol_73.model.dto.CreateStudentRequest;
 import com.sda.zd_javapol_73.model.dto.StudentDto;
 import com.sda.zd_javapol_73.model.mapper.StudentMapper;
+import com.sda.zd_javapol_73.repository.GradeRepository;
 import com.sda.zd_javapol_73.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -17,6 +22,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository studentRepository;
+    private final GradeRepository gradeRepository;
     private final StudentMapper studentMapper;
 
     public List<StudentDto> findAll(){
@@ -33,5 +39,16 @@ public class StudentService {
 
         student = studentRepository.save(student);
         return studentMapper.mapStudentToStudentDto(student);
+    }
+
+    public OptionalDouble calculateGradesAverage(Long studentId) {
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        if(studentOptional.isPresent()){
+            Student student = studentOptional.get();
+
+            List<Grade> grades = gradeRepository.findAllByStudent(student);
+            return grades.stream().mapToDouble(Grade::getGradeValue).average();
+        }
+        throw new EntityNotFoundException("Student with given id[" + studentId + "] does not exist!");
     }
 }
--- src/test/java/com/sda/zd_javapol_73/service/StudentServiceTest.java
+++ src/test/java/com/sda/zd_javapol_73/service/StudentServiceTest.java
@@ -1,7 +1,9 @@
 package com.sda.zd_javapol_73.service;
 
+import com.sda.zd_javapol_73.model.Grade;
 import com.sda.zd_javapol_73.model.Student;
 import com.sda.zd_javapol_73.model.dto.StudentDto;
+import com.sda.zd_javapol_73.repository.GradeRepository;
 import com.sda.zd_javapol_73.repository.StudentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -10,10 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -30,24 +31,37 @@ public class StudentServiceTest {
             new Student(5L, "Filip", "Myszka", "12345682", LocalDate.of(2018, 7, 21), new HashSet<>()),
             new Student(6L, "Felix", "Klawiatura", "12345683", LocalDate.of(2012, 1, 11), new HashSet<>())
     );
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @MockBean
+    private GradeRepository gradeRepository;
 
     @Autowired
     private StudentService studentService;
 
     @Nested
     class StudentServiceFetchStudentsTests {
-        @MockBean
-        private StudentRepository studentRepository;
-
         @BeforeEach
         void setUp() {
-            given(studentRepository.findAll()).willReturn(TESTING_SET);
+            // studentRepository.findAll() - obiekt i funkcja która ma być imitowana/sztuczna
+            given(studentRepository.findAll())
+                    .willReturn(TESTING_SET);
+        }
+
+        @Test
+        void findAll_repositoryIsCalledOneTime() {
+            // test weryfikujący że funkcja findAll jest wywoływana 1 raz.
+            studentService.findAll();
+
+            verify(studentRepository, times(1)).findAll();
         }
 
         @Test
         void findAll_returnsAllObjectsRetrievedFromDatabase() {
             List<StudentDto> studentDtoList = studentService.findAll();
 
+            verify(studentRepository, times(1)).findAll();
             assertNotNull(studentDtoList);
             assertEquals(TESTING_SET.size(), studentDtoList.size());
         }
@@ -67,6 +81,56 @@ public class StudentServiceTest {
                             studentDto.getSurname().equals(student.getLastName());
                 }));
             }
+        }
+    }
+
+    @Nested
+    class StudentServiceGradesCalculationTests {
+        @Test
+        void caluclateGradesAverage_handlesNoAverage() {
+            final long TEST_STUDENT_ID = 1L;
+            final Student testStudent = new Student(TEST_STUDENT_ID, "Marian", "Kowalski", "12345678", LocalDate.of(2011, 1, 8), new HashSet<>());
+
+            given(studentRepository.findById(TEST_STUDENT_ID)).willReturn(Optional.of(testStudent));
+
+            OptionalDouble optionalDouble = studentService.calculateGradesAverage(TEST_STUDENT_ID);
+            assertFalse(optionalDouble.isPresent());
+        }
+
+        @Test()
+        void caluclateGradesAverage_throwsExceptionIfGivenStudentDoesNotExist() {
+            final long TEST_STUDENT_ID = 2L;
+            given(studentRepository.findById(TEST_STUDENT_ID)).willReturn(Optional.empty());
+
+            assertThrows(EntityNotFoundException.class, () -> {
+                studentService.calculateGradesAverage(2L);
+            });
+        }
+
+        @Test()
+        void caluclateGradesAverage_properlyCalculatesAverage() {
+            final long TEST_STUDENT_ID = 3L;
+            final Student testStudent = new Student(TEST_STUDENT_ID,
+                    "Marian",
+                    "Kowalski",
+                    "12345678",
+                    LocalDate.of(2011, 1, 8),
+                    new HashSet<>(Arrays.asList(
+                            new Grade(null, null, 5.0, null),
+                            new Grade(null, null, 5.0, null),
+                            new Grade(null, null, 5.0, null),
+                            new Grade(null, null, 5.0, null),
+                            new Grade(null, null, 5.0, null)
+                    )));
+            given(studentRepository.findById(TEST_STUDENT_ID)).willReturn(Optional.of(testStudent));
+            given(gradeRepository.findAllByStudent(testStudent)).willReturn(new ArrayList<>(testStudent.getGrades()));
+
+            OptionalDouble optionalAverage = studentService.calculateGradesAverage(TEST_STUDENT_ID);
+            assertTrue(optionalAverage.isPresent());
+            assertEquals(5.0, optionalAverage.getAsDouble());
+
+            // jeśli spodziewamy się jakiegoś błędu obliczeń z akceptowalnym marginesem, to podajemy go jako 3 parametr.
+            assertEquals(5.0, optionalAverage.getAsDouble(), 0.001);
         }
     }
 
